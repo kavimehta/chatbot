a430694dfa8139969f751fa3dd9859f509c1d7dd
kavimehta <kavi.mehta2@gmail.com>
2016-06-28 14:36:00 -0700

    Added skeletons for store, writeDB, and loadDB

--- Chatbot.java
+++ Chatbot.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Chatbot {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		String call = "";
+		String response = "";
 		while (in.hasNext()) {
 			String phrase = in.nextLine();
 			if (phrase.equals("quit")) {
@@ -10,8 +12,23 @@ public class Chatbot {
 				in.close();
 				break;
 			} else {
+				response = phrase;
+				store(call, response);
 				System.out.println("Roger: " + phrase);
+				call = phrase;
 			}
 		}
+	}
+
+	private static void store(String call, String response) {
+
+	}
+
+	private static void writeDB() {
+
+	}
+
+	private static void loadDB() {
+		
 	}
 }
